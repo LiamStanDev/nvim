b5dd357ab1189e85db4f2ba10987105ab4954115
liam <geffc1454@gmail.com>
2023-05-06 20:12:41 +0800

    [fix] bufferline color

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -3,6 +3,8 @@ M.setup = function()
 	local g_configs = require("core")
 	local colorscheme = g_configs.colorscheme or "onedark"
 
+	-- set catppuccin first for bufferline color(can't delete)
+	local _, _ = pcall(vim.cmd, "colorscheme " .. "catppuccin")
 	local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 	if not status_ok then
 		vim.notify("colorscheme " .. colorscheme .. " not found!")
