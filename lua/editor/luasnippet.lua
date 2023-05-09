2c3c4eb98f801c7678b7c160ed1c426f424096a2
liam <geffc1454@gmail.com>
2023-05-09 11:45:22 +0800

    [fix] luasnipt don't show the html snipt in tsx and jsx

--- lua/editor/luasnippet.lua
+++ lua/editor/luasnippet.lua
@@ -1,9 +1,18 @@
 return function()
-	require("luasnip").setup({
+	local snippet_path = vim.fn.stdpath("config") .. "/snips/"
+
+	if not vim.tbl_contains(vim.opt.rtp:get(), snippet_path) then
+		vim.opt.rtp:append(snippet_path)
+	end
+	local luasnip = require("luasnip")
+	luasnip.setup({
 		history = true,
 		updateevents = "TextChanged,TextChangedI",
 		delete_check_events = "TextChanged,InsertLeave",
 	})
+
+	-- let one file can use multiple snippet sources.
+	luasnip.filetype_extend("typescriptreact", { "html", "javascript" })
 
 	require("luasnip.loaders.from_lua").lazy_load()
 	require("luasnip.loaders.from_vscode").lazy_load()
