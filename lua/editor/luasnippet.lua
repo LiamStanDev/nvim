0b5ca197d14d1d06c8657626ee9796a321b22ae3
liam <geffc1454@gmail.com>
2023-05-11 19:23:24 +0800

    [new] can config my won snippets

--- lua/editor/luasnippet.lua
+++ lua/editor/luasnippet.lua
@@ -1,22 +1,36 @@
 return function()
-	local snippet_path = vim.fn.stdpath("config") .. "/snips/"
-
-	if not vim.tbl_contains(vim.opt.rtp:get(), snippet_path) then
-		vim.opt.rtp:append(snippet_path)
-	end
 	local luasnip = require("luasnip")
 	luasnip.setup({
 		history = true,
 		updateevents = "TextChanged,TextChangedI",
 		delete_check_events = "TextChanged,InsertLeave",
+		-- ext_opts = {
+		-- 	[require("luasnip.util.types").choiceNode] = {
+		-- 		activt = {
+		-- 			virt_text = { { "●", "GruvboxOrange" } },
+		-- 		},
+		-- 	},
+		-- },
 	})
 
 	-- let one file can use multiple snippet sources.
-	luasnip.filetype_extend("typescriptreact", { "html", "javascript" })
+	luasnip.filetype_extend("typescriptreact", { "html" })
 
-	require("luasnip.loaders.from_lua").lazy_load()
-	require("luasnip.loaders.from_vscode").lazy_load()
-	require("luasnip.loaders.from_snipmate").lazy_load({
-		paths = { "./snips/" },
-	})
+	require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets/" } })
+	require("luasnip.loaders.from_vscode").load({ exclude = { "typescriptreact", "cs" } })
+
+	-- key map
+	local keymap = vim.keymap.set
+	-- next
+	keymap({ "i", "s" }, "<C-.>", function()
+		if luasnip.expand_or_jumpable() then
+			luasnip.expand_or_jump()
+		end
+	end)
+	-- prev
+	keymap({ "i", "s" }, "<C-,>", function()
+		if luasnip.jumpable(-1) then
+			luasnip.jump(-1)
+		end
+	end)
 end
