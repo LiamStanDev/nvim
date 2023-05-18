528bc69115b6c9169673c6e1d3499ba245783146
liam <geffc1454@gmail.com>
2023-05-18 13:47:44 +0800

    [fix] add hop.nvim config file

--- lua/plugin-list.lua
+++ lua/plugin-list.lua
@@ -263,10 +263,7 @@ M.plugins = {
 	{
 		"phaazon/hop.nvim",
 		branch = "v2", -- optional but strongly recommended
-		config = function()
-			-- you can configure Hop the way you like here; see :h hop-config
-			require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
-		end,
+		config = require("tools.hop"),
 	},
 
 	-- telescopte
