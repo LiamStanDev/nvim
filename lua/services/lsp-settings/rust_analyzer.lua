e0c41caae6b15a7448bb484b4fe2a008eb38ac1d
Liam Lin <geffc1454@gmail.com>
2023-05-30 09:14:01 +0800

    [add] enhance rust-analyzer

--- lua/services/lsp-settings/rust_analyzer.lua
+++ lua/services/lsp-settings/rust_analyzer.lua
@@ -5,8 +5,23 @@ return {
 				enable = true,
 			},
 			checkOnSave = true,
+			check = {
+				command = "check",
+				allTargets = true,
+				features = "all",
+				invocationLocation = "workspace",
+				extraArgs = {},
+			},
 			cargo = {
 				allFeatures = true,
+			},
+			inlayHints = {
+				closureCaptureHints = {
+					enable = true,
+				},
+				closureReturnTypeHints = {
+					enable = true,
+				},
 			},
 		},
 	},
