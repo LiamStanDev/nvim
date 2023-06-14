fc2f28f3baee6b8aa22e3c6fe5431ac150cd79dd
Liam Lin <geffc1454@gmail.com>
2023-06-14 23:27:48 +0800

    [new] add autoclose

--- lua/editor/autoclose.lua
+++ lua/editor/autoclose.lua
@@ -5,19 +5,19 @@ return function()
 			disable_when_touch = true,
 		},
 		keys = {
-			["("] = { escape = false, close = true, pair = "()" },
-			["["] = { escape = false, close = true, pair = "[]" },
-			["{"] = { escape = false, close = true, pair = "{}" },
-			-- ["<"] = { escape = false, close = true, pair = "<>" },
-
-			[">"] = { escape = true, close = false, pair = "<>" },
-			[")"] = { escape = true, close = false, pair = "()" },
-			["]"] = { escape = true, close = false, pair = "[]" },
-			["}"] = { escape = true, close = false, pair = "{}" },
-
-			['"'] = { escape = true, close = true, pair = '""' },
-			["'"] = { escape = true, close = true, pair = "''" },
-			["`"] = { escape = true, close = true, pair = "``" },
+			-- ["("] = { escape = false, close = true, pair = "()" },
+			-- ["["] = { escape = false, close = true, pair = "[]" },
+			-- ["{"] = { escape = false, close = true, pair = "{}" },
+			-- -- ["<"] = { escape = false, close = true, pair = "<>" },
+			--
+			-- [">"] = { escape = true, close = false, pair = "<>" },
+			-- [")"] = { escape = true, close = false, pair = "()" },
+			-- ["]"] = { escape = true, close = false, pair = "[]" },
+			-- ["}"] = { escape = true, close = false, pair = "{}" },
+			--
+			-- ['"'] = { escape = true, close = true, pair = '""' },
+			-- ["'"] = { escape = true, close = true, pair = "''" },
+			-- ["`"] = { escape = true, close = true, pair = "``" },
 			["|"] = { escape = true, close = true, pair = "||" },
 		},
 	})
